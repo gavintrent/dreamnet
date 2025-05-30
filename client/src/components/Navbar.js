3318d55aeddb4e7acf0f9505c92d19021a20fd03
gavintrent <gavintrent@g.ucla.edu>
2025-05-29 22:03:19 -0700

    zZz animation over dreamNet

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -81,14 +81,15 @@ export default function Navbar({ loggedIn, onLogout, currentUser}) {
             </div>
         </div>
 
-        <div className="relative-inline-block">
+        <div className="relative inline-block">
             <Link to="/" className="btn btn-ghost text-2xl normal-case font-pixelify-italic animate-pulse [animation-duration:3s] ease-in-out [color:#d40f95]">
               DreamNet
             </Link>
-            <div className="absolute -top-6 left-full ml-2 flex flex-col items-center space-y-[-0.2rem] pointer-events-none select-none z-10">
-              <span className="animate-z1 text-xs text-white">Z</span>
-              <span className="animate-z2 text-sm text-white">Z</span>
-              <span className="animate-z3 text-base text-white">Z</span>
+            <div className="absolute -top-6 left-[110%] ml-[-1.7rem] flex flex-col items-center space-y-[-0.5rem] pointer-events-none select-none z-10">
+              <span className="animate-z1 text-[1rem] text-white translate-x-1 font-pixelify">Z</span>
+              <span className="animate-z2 text-[0.8rem] text-white translate-x-0 font-pixelify">Z</span>
+              <span className="animate-z3 text-[0.6rem] text-white translate-x-[-0.5rem] font-pixelify">Z</span>
+              <span className="animate-z4"></span>
             </div>
         </div>
 

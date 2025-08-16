ee780b182bd07c671f60e7088472a221adf6ff8e
gavintrent <gavintrent@g.ucla.edu>
2025-08-16 14:28:35 -0700

    fix misaligned ZZZ animation

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -11,7 +11,7 @@ export default function Logo() {
       >
         DreamNet
       </Link>
-      <div className="absolute -top-6 left-[110%] ml-[-1.7rem] flex flex-col items-center space-y-[-0.5rem] pointer-events-none select-none z-10">
+      <div className="absolute -top-6 left-full ml-2 flex flex-col items-center space-y-[-0.5rem] pointer-events-none select-none z-10">
         <span className="animate-z1 text-[1rem] text-white translate-x-1 jersey-10-regular">Z</span>
         <span className="animate-z2 text-[0.8rem] text-white translate-x-0 jersey-10-regular">Z</span>
         <span className="animate-z3 text-[0.6rem] text-white translate-x-[-0.5rem] jersey-10-regular">Z</span>

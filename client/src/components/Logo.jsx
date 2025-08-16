f4a7af2d34fda5cd48a702ce8b2843f2971876da
gavintrent <gavintrent@g.ucla.edu>
2025-08-16 15:46:01 -0700

    Logo alignment issues fix

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -7,11 +7,11 @@ export default function Logo() {
     <div className="relative inline-block">
       <Link
         to="/"
-        className="inline-flex items-center justify-center px-6 py-3 bg-transparent shadow-none border-none rounded-full text-3xl normal-case font-pixelify-italic animate-pulse [animation-duration:4s] ease-in-out text-highlight hover:opacity-80 transition-opacity duration-200"
+        className="inline-flex items-center justify-center px-2 py-3 bg-transparent shadow-none border-none rounded-full text-3xl normal-case font-pixelify-italic animate-pulse [animation-duration:4s] ease-in-out text-highlight hover:opacity-80 transition-opacity duration-200"
       >
         DreamNet
       </Link>
-      <div className="absolute -top-6 left-full ml-2 flex flex-col items-center space-y-[-0.5rem] pointer-events-none select-none z-10">
+      <div className="absolute -top-4 left-full flex flex-col items-center space-y-[-0.5rem] pointer-events-none select-none z-10">
         <span className="animate-z1 text-[1rem] text-white translate-x-1 jersey-10-regular">Z</span>
         <span className="animate-z2 text-[0.8rem] text-white translate-x-0 jersey-10-regular">Z</span>
         <span className="animate-z3 text-[0.6rem] text-white translate-x-[-0.5rem] jersey-10-regular">Z</span>
